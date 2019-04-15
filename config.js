fac5fab27551a755319fcc4ac74de75756f1bc95
pardhu225 <chowdary1969@gmail.com>
2019-04-15 09:44:43 +0530

    done?

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports.electionId = " u8aosid;oias9 3io20p8asd fg jkl;s89q2w y9peos r9p sdafnjlkl;sdf";
+
+module.exports.privateKeyFile = 'linux_point.pem';
+
+module.exports.usernameLength = 32;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+const NodeRSA = require('node-rsa');
+const config = require('./config');
+const utils = require('./utils');
+const crypto = require('crypto');
+
+var pemKey = false;
+
+app.get('/get-credentials', (req, res) => {
+    if (!pemKey) {
+        res.status(500).json({message: 'Server not ready'});
+        return;
+    }
+    var u = utils.makeUsername(config.usernameLength);
+    var p = pemKey.encrypt(u + config.electionId, 'base64');
+    // p = crypto.createHmac('sha256', pwd).digest('hex');
+    res.status(200).json({ u, p });
+});
+
+app.get('/get-pubkey', (req, res) => {
+    if (!pemKey) {
+        res.status(500).json({message: 'Server not ready'});
+        return;
+    }
+    res.status(200).json({ key: pemKey.exportKey('public') });
+});
+
+fs.readFile(config.privateKeyFile, (err, data) => {
+    if (err) {
+        console.log('Unable to load pem file')
+        process.exit(1);
+    }
+    pemKey = new NodeRSA(data);
+});
+
+app.listen(port, () => console.log(`Trusted Organization app listening on port ${port}!`))
